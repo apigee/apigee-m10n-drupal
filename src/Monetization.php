560b44f264e85aa641fac384e78b87b177b515d3
Theo Ballew <theoballew@gmail.com>
2018-08-31 22:07:45 -0700

    [DRUP-337] add caching, messaging, and tests to Monetization::isMonetizationEnabled method

--- src/Monetization.php
+++ src/Monetization.php
@@ -1,28 +1,84 @@
 <?php
 
+/**
+ * Copyright 2018 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 namespace Drupal\apigee_m10n;
 
 use Apigee\Edge\Api\Management\Controller\OrganizationController;
 use Drupal\apigee_edge\SDKConnectorInterface;
+use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Messenger\MessengerInterface;
 
 class Monetization implements MonetizationInterface {
 
+  /**
+   * @var \Drupal\apigee_edge\SDKConnectorInterface
+   */
   private $sdk_connector;
 
-  public function __construct(SDKConnectorInterface $sdk_connector) {
+  /**
+   * Drupal core messenger service (for adding flash messages).
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  private $messenger;
+
+  /**
+   * @var \Drupal\Core\Cache\CacheBackendInterface
+   */
+  private $cache;
+
+  /**Ò
+   * Monetization constructor.
+   *
+   * @param \Drupal\apigee_edge\SDKConnectorInterface $sdk_connector
+   */
+  public function __construct(SDKConnectorInterface $sdk_connector, MessengerInterface $messenger, CacheBackendInterface $cache) {
     $this->sdk_connector = $sdk_connector;
+    $this->messenger     = $messenger;
+    $this->cache         = $cache;
   }
 
   /**
    * {@inheritdoc}
    */
   public function isMonetizationEnabled(): bool {
-    $org_controller = new OrganizationController($this->sdk_connector->getClient());
     $org_id = $this->sdk_connector->getOrganization();
-    $org = $org_controller->load($org_id);
 
-    $is_monetization_enabled = $org->getPropertyValue('features.isMonetizationEnabled');
+    $monetization_status = $this->cache->get("apigee_m10n:org_monetization_status:{$org_id}");
 
-    return (bool) $is_monetization_enabled;
+    if (!$monetization_status) {
+
+      $org_controller = new OrganizationController($this->sdk_connector->getClient());
+
+      try {
+        $org = $org_controller->load($org_id);
+      } catch (\Exception $e) {
+        $this->messenger->addError($e->getMessage());
+        return $is_monetization_enabled = FALSE;
+      }
+
+      $monetization_status = $org->getPropertyValue('features.isMonetizationEnabled') === 'true' ? 'enabled' : 'disabled';
+      $expire_time         = new \DateTime('now + 5 minutes');
+
+      $this->cache->set("apigee_m10n:org_monetization_status:{$org_id}", $monetization_status, $expire_time->getTimestamp());
+    }
+
+    return (bool) $is_monetization_enabled = $monetization_status === 'enabled' ? TRUE : FALSE;
   }
 }
---
+++ tests/src/Kernel/MonetizationServiceKernelTest.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Copyright 2018 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+namespace Drupal\Tests\apigee_m10n\Kernel;
+
+use GuzzleHttp\Psr7\Response;
+
+/**
+ * Tests the testing framework for testing offline.
+ *
+ * @group apigee_m10n
+ */
+class MonetizationServiceKernelTest extends M10nKernelTestBase {
+
+  /**
+   * @var \Drupal\apigee_m10n\MonetizationInterface
+   */
+  protected $monetization;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->monetization = $this->container->get('apigee_m10n.monetization');
+  }
+
+  /**
+   * Tests that monetization service correctly identifies a monetized
+   * organization.
+   */
+  public function testMonetizationEnabled() {
+
+    // Response body representing a monetized organization.
+    $response_body = <<< EOF
+      {
+        "properties" : {
+          "property" : [ {
+              "name" : "features.isMonetizationEnabled",
+              "value" : "true"
+            }
+          ]
+        }
+      }
+EOF;
+
+    // Queue a response from the mock server.
+    $this->stack->append(new Response(200, [], $response_body));
+
+    // Execute a client call.
+    $is_monetization_enabled = $this->monetization->isMonetizationEnabled();
+
+
+    self::assertEquals($is_monetization_enabled, TRUE);
+  }
+
+  /**
+   * Tests that the monetization service correctly identifies an organization without monetization enabled.
+   */
+  public function testMonetizationDisabled() {
+
+    // Response body representing a monetized organization.
+    $response_body = <<< EOF
+      {
+        "properties" : {
+          "property" : [ {
+              "name" : "features.isMonetizationEnabled",
+              "value" : "false"
+            }
+          ]
+        }
+      }
+EOF;
+
+    // Queue a response from the mock server.
+    $this->stack->append(new Response(200, [], $response_body));
+
+    // Execute a client call.
+    $is_monetization_enabled = $this->monetization->isMonetizationEnabled();
+
+
+    self::assertEquals($is_monetization_enabled, FALSE);
+  }
+}
