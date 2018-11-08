a9415bb53c38325322d46bc949365f04959f9186
Theo Ballew <theoballew@gmail.com>
2018-11-08 14:43:21 -0800

    [DRUP-382] initial tests

---
+++ tests/src/Functional/SubscriptionListTest.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Copyright 2018 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+namespace Drupal\Tests\apigee_m10n\Functional;
+
+use Drupal\Core\Url;
+
+/**
+ * Class SubscriptionListTest
+ *
+ * @group apigee_m10n
+ */
+class SubscriptionListTest extends MonetizationFunctionalTestBase {
+
+  /**
+   * Drupal user.
+   *
+   * @var $account
+   */
+  protected $account;
+
+  public function testSubscriptionListAccessDenied() {
+
+    // If the user doesn't have the "access subscriptions" permission, they should get access denied.
+    $this->account = $this->createAccount([]);
+
+    $this->queueOrg();
+
+    $this->drupalLogin($this->account);
+    $this->drupalGet(Url::fromRoute('entity.subscription.collection_by_developer', [
+      'user' => $this->account->id(),
+    ]));
+
+    $this->assertSession()->responseContains('Access denied');
+  }
+
+  public function testSubscriptionListView() {
+    // If the user has "access subscriptions" permission, they should be able to see some prepaid balances.
+    $this->account = $this->createAccount([
+      'view mint prepaid reports'
+    ]);
+
+    $this->queueOrg();
+
+    $this->drupalLogin($this->account);
+
+    $subscriptions = [
+      $this->createSubscription(),
+      $this->createSubscription(),
+    ];
+
+    $this->stack->queueFromResponseFile(['get-prepaid-balances']);
+
+    $this->drupalGet(Url::fromRoute('apigee_monetization.billing', [
+      'user' => $this->account->id()
+    ]));
+
+  }
+}
