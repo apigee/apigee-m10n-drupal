803e8eed24589222395ef8ab60ce07cbe1cba229
Jaesin Mulenex <Jaesin@users.noreply.github.com>
2018-11-22 02:06:01 -0800

    [DRUP-382] Remove field tabs from the `rate_plan` admin section.

---
+++ src/Routing/MonetizationRouteSubscriber.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+namespace Drupal\apigee_m10n\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Drupal\Core\Routing\RoutingEvents;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Subscriber for Monetization routes.
+ */
+class MonetizationRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    foreach (['rate_plan'] as $entity_type_id) {
+      $collection->get("entity.field_config.{$entity_type_id}_field_edit_form")->setRequirements(['_access' => 'FALSE']);
+      $collection->get("entity.field_config.{$entity_type_id}_storage_edit_form")->setRequirements(['_access' => 'FALSE']);
+      $collection->get("entity.field_config.{$entity_type_id}_field_delete_form")->setRequirements(['_access' => 'FALSE']);
+      $collection->get("entity.{$entity_type_id}.field_ui_fields")->setRequirements(['_access' => 'FALSE']);
+      $collection->get("field_ui.field_storage_config_add_{$entity_type_id}")->setRequirements(['_access' => 'FALSE']);
+      $collection->get("entity.entity_form_display.{$entity_type_id}.default")->setRequirements(['_access' => 'FALSE']);
+      $collection->get("entity.entity_form_display.{$entity_type_id}.form_mode")->setRequirements(['_access' => 'FALSE']);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events = parent::getSubscribedEvents();
+    $events[RoutingEvents::ALTER] = ['onAlterRoutes', -101];
+    return $events;
+  }
+
+}
