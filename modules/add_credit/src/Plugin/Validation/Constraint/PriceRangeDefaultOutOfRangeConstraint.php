ee10ab01416fd8e9598baf82186eac0075fb4fda
Arshad Chummun <arshad@chummun.com>
2019-01-22 15:16:17 +0400

    [DRUP 386] Add a price range field with validation

---
+++ modules/add_credit/src/Plugin/Validation/Constraint/PriceRangeDefaultOutOfRangeConstraint.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+namespace Drupal\apigee_m10n_add_credit\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Validates the default value range.
+ *
+ * @Constraint(
+ *   id = "PriceRangeDefaultOutOfRange",
+ *   label = @Translation("The default value is out of range.", context = "Validation"),
+ * )
+ */
+class PriceRangeDefaultOutOfRangeConstraint extends Constraint {
+
+  public $message = 'The default value is out of range.';
+
+}
