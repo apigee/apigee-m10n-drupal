df9bc19245c7fc0acd0ef7d468110f21cafb2af4
Minnur Yunusov <mmyunusov@gmail.com>
2019-02-28 15:10:02 -0800

    [DRUP-563] Tests placeholder.

---
+++ modules/apigee_m10n_teams/tests/src/Functional/BillingDetailsTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Copyright 2018 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+namespace Drupal\Tests\apigee_m10n_teams\Functional;
+
+use Drupal\Core\Url;
+
+/**
+ * Class BillingDetailsTest.
+ *
+ * @group apigee_m10n_teams
+ * @group apigee_m10n_teams_functional
+ */
+class BillingDetailsTest extends MonetizationTeamsFunctionalTestBase {
+
+  /**
+   * Drupal user.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $developer;
+
+  /**
+   * {@inheritdoc}
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   * @throws \Twig_Error_Loader
+   * @throws \Twig_Error_Runtime
+   * @throws \Twig_Error_Syntax
+   */
+  public function setUp() {
+    parent::setUp();
+    $this->developer = $this->createAccount([]);
+    $this->drupalLogin($this->developer);
+  }
+
+  /**
+   * Tests for `Billing Details` page.
+   *
+   * @throws \Exception
+   */
+  public function testBillingDetailsPageView() {
+  }
+
+}
---
+++ modules/apigee_m10n_teams/tests/src/Functional/MonetizationTeamsFunctionalTestBase.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+namespace Drupal\Tests\apigee_m10n_teams\Functional;
+
+use Drupal\Tests\apigee_m10n\Traits\ApigeeMonetizationTestTrait;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * A base class for functional tests.
+ */
+class MonetizationTeamsFunctionalTestBase extends BrowserTestBase {
+
+  use ApigeeMonetizationTestTrait {
+    setUp as baseSetUp;
+  }
+
+  protected static $modules = [
+    'apigee_m10n_test',
+    'apigee_mock_client',
+    'system',
+    'apigee_edge',
+    'apigee_edge_teams',
+    'apigee_m10n',
+    'apigee_m10n_teams',
+  ];
+
+  /**
+   * The mock handler stack is responsible for serving queued api responses.
+   *
+   * @var \Drupal\apigee_mock_client\MockHandlerStack
+   */
+  protected $stack;
+
+  /**
+   * Whether actual integration tests are enabled.
+   *
+   * @var bool
+   */
+  protected $integration_enabled;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    // Create new Apigee Edge basic auth key.
+    $this->baseSetUp();
+  }
+
+}
