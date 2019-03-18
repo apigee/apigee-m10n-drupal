17f55a3fc7687ab37d49549673b8884ffa561780
Jaesin Mulenex <jaesin@no-reply.gradientsky.com>
2019-03-19 03:26:06 +0700

    [DRUP-568] Reduce a bit of complexity in the TnC widget.

--- modules/apigee_m10n_teams/src/Plugin/Field/FieldWidget/CompanyTermsAndConditionsWidget.php
+++ modules/apigee_m10n_teams/src/Plugin/Field/FieldWidget/CompanyTermsAndConditionsWidget.php
@@ -20,15 +20,10 @@
 namespace Drupal\apigee_m10n_teams\Plugin\Field\FieldWidget;
 
 use Drupal\apigee_m10n\Plugin\Field\FieldWidget\TermsAndConditionsWidget;
-use Drupal\apigee_m10n_teams\Entity\TeamRouteAwareSubscriptionInterface;
 use Drupal\apigee_m10n_teams\MonetizationTeamsInterface;
-use Drupal\apigee_m10n\MonetizationInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
-use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Url;
-use Drupal\Core\Link;
 
 /**
  * Override class for the `apigee_tnc_widget` field widget.
@@ -36,25 +31,11 @@ use Drupal\Core\Link;
 class CompanyTermsAndConditionsWidget extends TermsAndConditionsWidget {
 
   /**
-   * Company ID.
-   *
-   * @var string
-   */
-  protected $company_id;
-
-  /**
    * Teams Monetization factory.
    *
    * @var \Drupal\apigee_m10n_teams\MonetizationTeamsInterface
    */
   protected $team_monetization;
-
-  /**
-   * Monetization factory.
-   *
-   * @var \Drupal\apigee_m10n\MonetizationInterface
-   */
-  protected $monetization;
 
   /**
    * SubscribeLinkFormatter constructor.
@@ -71,13 +52,10 @@ class CompanyTermsAndConditionsWidget extends TermsAndConditionsWidget {
    *   Any third party settings.
    * @param \Drupal\apigee_m10n_teams\MonetizationTeamsInterface $team_monetization
    *   Teams monetization factory.
-   * @param \Drupal\apigee_m10n\MonetizationInterface $monetization
-   *   Monetization factory.
    */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, array $third_party_settings, MonetizationTeamsInterface $team_monetization = NULL, MonetizationInterface $monetization = NULL) {
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, array $third_party_settings, MonetizationTeamsInterface $team_monetization) {
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings);
     $this->team_monetization = $team_monetization;
-    $this->monetization = $monetization;
   }
 
   /**
@@ -90,39 +68,8 @@ class CompanyTermsAndConditionsWidget extends TermsAndConditionsWidget {
       $configuration['field_definition'],
       $configuration['settings'],
       $configuration['third_party_settings'],
-      $container->get('apigee_m10n.teams'),
-      $container->get('apigee_m10n.monetization')
+      $container->get('apigee_m10n.teams')
     );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
-    $entity = $items->getEntity();
-    if ($entity instanceof TeamRouteAwareSubscriptionInterface && $entity->isTeamSubscription())  {
-      // We won't ask a user to accept terms and conditions again if it has been already accepted.
-      $this->company_id = $entity->decorated()->getCompany()->id();
-      if (!$items[$delta]->value) {
-        $tnc = $this->monetization->getLatestTermsAndConditions();
-        $element += [
-          '#type'             => 'checkbox',
-          '#default_value'    => !empty($items[0]->value),
-          '#element_validate' => [[$this, 'validate']],
-        ];
-        // Accept TnC description.
-        $element['#description'] = $this->t('%description @link', [
-          '%description' => ($description = $tnc->getDescription()) ? $this->t($description) : $this->getSetting('default_description'),
-          '@link' => ($link = $tnc->getUrl()) ? Link::fromTextAndUrl($this->t('Terms and Conditions'), Url::fromUri($link))->toString() : '',
-        ]);
-        $element['#attached']['library'][] = 'apigee_m10n/tnc-widget';
-        return ['value' => $element];
-      }
-    }
-    else {
-      return parent::formElement($items, $delta, $element, $form, $form_state);
-    }
-
   }
 
   /**
@@ -136,7 +83,7 @@ class CompanyTermsAndConditionsWidget extends TermsAndConditionsWidget {
     }
     else {
       // Accept terms and conditions.
-      $this->team_monetization->acceptLatestTermsAndConditions($this->company_id);
+      $this->team_monetization->acceptLatestTermsAndConditions($this->entity->getCompany()->id());
     }
   }
 
