2f4355bf1218e94f39f4dd34c0be6afd176ad055
arshad <arshad@chummun.com>
2020-04-13 13:37:22 +0400

    [#227] Implement test payment gateway requirement

--- modules/apigee_m10n_add_credit/src/Plugin/Requirement/Requirement/PaymentGateway.php
+++ modules/apigee_m10n_add_credit/src/Plugin/Requirement/Requirement/PaymentGateway.php
@@ -19,9 +19,11 @@
 
 namespace Drupal\apigee_m10n_add_credit\Plugin\Requirement\Requirement;
 
-use Drupal\Core\Link;
-use Drupal\Core\Url;
+use Drupal\commerce_payment\PaymentGatewayManager;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\requirement\Plugin\RequirementBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Check that at least a commerce payment gateway exists.
@@ -35,32 +37,97 @@ use Drupal\requirement\Plugin\RequirementBase;
  *   severity="error"
  * )
  */
-class PaymentGateway extends RequirementBase {
+class PaymentGateway extends RequirementBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The payment gateway plugin manager.
+   *
+   * @var \Drupal\commerce_payment\PaymentGatewayManager
+   */
+  protected $paymentGatewayManager;
+
+  /**
+   * PaymentGateway constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\commerce_payment\PaymentGatewayManager $payment_gateway_manager
+   *   The payment gateway plugin manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, PaymentGatewayManager $payment_gateway_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->paymentGatewayManager = $payment_gateway_manager;
+  }
 
   /**
    * {@inheritdoc}
    */
-  public function getActionButton(): array {
-    // Return a link to the payment gateway page.
-    if (($entity_type = $this->entityTypeManager->getDefinition('commerce_payment_gateway')) && ($uri = $entity_type->getLinkTemplate('add-form'))) {
-      return Link::fromTextAndUrl($this->getActionButtonLabel(), Url::fromUserInput($uri, [
-        'query' => \Drupal::destination()->getAsArray(),
-        'attributes' => [
-          'class' => [
-            'button',
-          ],
-        ],
-      ]))->toRenderable();
-    }
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('plugin.manager.commerce_payment_gateway')
+    );
+  }
 
-    return parent::getActionButton();
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state): array {
+    $form['gateway'] = [
+      '#markup' => $this->t('Configure a payment gateway to handle prepaid balance checkouts.'),
+    ];
+
+    $form['label'] = [
+      '#title' => $this->t('Name'),
+      '#type' => 'textfield',
+      '#maxlength' => 255,
+      '#required' => TRUE,
+      '#default_value' => $this->t('Test gateway'),
+    ];
+
+    $form['id'] = [
+      '#type' => 'machine_name',
+      '#machine_name' => [
+        'exists' => '\Drupal\commerce_payment\Entity\PaymentGateway::load',
+      ],
+    ];
+
+    $form['plugin'] = [
+      '#type' => 'hidden',
+      '#value' => 'manual',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $form_state->cleanValues();
+
+    try {
+      $gateway = $this->getEntityTypeManager()
+        ->getStorage('commerce_payment_gateway')
+        ->create($form_state->getValues());
+      $gateway->save();
+    }
+    catch (\Exception $exception) {
+      watchdog_exception('apigee_m10n_add_credit', $exception);
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function isApplicable(): bool {
-    return $this->getModuleHandler()->moduleExists('apigee_m10n_add_credit');
+    return $this->getModuleHandler()->moduleExists('apigee_m10n_add_credit') && !empty($this->paymentGatewayManager->getDefinitions());
   }
 
   /**
