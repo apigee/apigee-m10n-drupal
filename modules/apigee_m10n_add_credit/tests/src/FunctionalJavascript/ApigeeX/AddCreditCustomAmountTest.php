4fcee00376785ce2408ceb4ea509814de922b2c2
phdhiren <hirenkumar@google.com>
2022-05-05 14:29:18 +0530

    Removed testPriceFieldOnDefaultProduct for ApigeeX
    Removed testPriceFieldOnDefaultProduct for ApigeeX as that irrelevant
    https://github.com/apigee/apigee-m10n-drupal/blob/b8251c000634658c038cb01d915e684d8dbcf9c7/modules/apigee_m10n_add_credit/src/AddCreditService.php#L151

--- modules/apigee_m10n_add_credit/tests/src/FunctionalJavascript/ApigeeX/AddCreditCustomAmountTest.php
+++ modules/apigee_m10n_add_credit/tests/src/FunctionalJavascript/ApigeeX/AddCreditCustomAmountTest.php
@@ -293,36 +293,6 @@ class AddCreditCustomAmountTest extends AddCreditFunctionalJavascriptTestBase {
   }
 
   /**
-   * Make sure that the price field does not break if price range is not used.
-   *
-   * @param mixed $value
-   *   The value for the price field.
-   * @param string $message
-   *   The expected message.
-   *
-   * @throws \Behat\Mink\Exception\ResponseTextException
-   *
-   * @dataProvider providerPriceField
-   */
-  public function testPriceFieldOnDefaultProduct($value, string $message) {
-    $this->enableProductVariationsField();
-
-    $this->drupalGet('product/add/default');
-
-    $title = 'Name of product';
-    $this->submitForm([
-      'title[0][value]' => $title,
-      'variations[form][0][sku][0][value]' => 'SKU-PRODUCT',
-      'variations[form][0][price][0][number]' => $value,
-      'variations[form][0][apigee_price_range][0][price_range][fields][minimum]' => 10.00,
-    ], 'Save');
-
-    $this->assertSession()->pageTextContains(t($message, [
-      '@title' => $title,
-    ]));
-  }
-
-  /**
    * Provides data to self::testPriceRangeFieldValidation().
    */
   public function providerPriceRange() {
