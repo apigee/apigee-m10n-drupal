d646e18564d049af57622e0dda19bda89f153948
Kedar Khaire <kwkhaire11@gmail.com>
2025-02-20 16:10:49 +0530

    Datetime description updated

--- src/Plugin/Field/FieldWidget/DatestampWidget.php
+++ src/Plugin/Field/FieldWidget/DatestampWidget.php
@@ -38,11 +38,12 @@ use Drupal\Core\Form\FormStateInterface;
 class DatestampWidget extends TimestampDatetimeWidget {
 
   /**
-   * Example format used as Datetime::formatExample is removed from D11.
+   * Example format used as Datetime::formatExample is deprecated 
+   * in Drupal:10.2.0 and is removed from Drupal:11.0.0.
    *
    * @var string
    */
-  public const DATESTAMPFORMAT = 'YYYY-MM-DD hh:mm:ss';
+  public const DATESTAMPFORMAT = 'YYYY-MM-DD HH:MM:SS';
 
   /**
    * {@inheritdoc}
