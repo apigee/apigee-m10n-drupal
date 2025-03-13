821b7b4e72ff653df720c93b51d69db8b8948dda
Kedar Khaire <kwkhaire11@gmail.com>
2025-03-13 14:43:29 +0530

    Check ROute failure

--- tests/src/Kernel/Access/AccessKernelTest.php
+++ tests/src/Kernel/Access/AccessKernelTest.php
@@ -314,20 +314,44 @@ class AccessKernelTest extends MonetizationKernelTestBase {
    */
   public function assertAdminRoutes() {
     /** @var \Drupal\Core\Url[] $admin_routes */
-    $admin_routes = [
-      Url::fromRoute('apigee_m10n.settings'),
-      Url::fromRoute('apigee_m10n.settings.rate_plan'),
-      Url::fromRoute('apigee_m10n.settings.prepaid_balance'),
-      Url::fromRoute('entity.entity_view_display.purchased_plan.default'),
-      Url::fromRoute('entity.product_bundle.collection'),
-    ];
+    // $admin_routes = [
+    //   Url::fromRoute('apigee_m10n.settings'),
+    //   Url::fromRoute('apigee_m10n.settings.rate_plan'),
+    //   Url::fromRoute('apigee_m10n.settings.prepaid_balance'),
+    //   Url::fromRoute('entity.entity_view_display.purchased_plan.default'),
+    //   Url::fromRoute('entity.product_bundle.collection'),
+    // ];
+    $route1 = Url::fromRoute('apigee_m10n.settings');
+    static::assertTrue($route1->access($this->administrator));
+    static::assertFalse($route1->access($this->developer));
+    static::assertFalse($route1->access($this->anonymous));
+
+    $route2 = Url::fromRoute('apigee_m10n.settings.rate_plan');
+    static::assertTrue($route2->access($this->administrator));
+    static::assertFalse($route2->access($this->developer));
+    static::assertFalse($route2->access($this->anonymous));
+
+    $route3 = Url::fromRoute('apigee_m10n.settings.prepaid_balance');
+    static::assertTrue($route3->access($this->administrator));
+    static::assertFalse($route3->access($this->developer));
+    static::assertFalse($route3->access($this->anonymous));
+
+    $route4 = Url::fromRoute('entity.entity_view_display.purchased_plan.default');
+    static::assertTrue($route4->access($this->administrator));
+    static::assertFalse($route4->access($this->developer));
+    static::assertFalse($route4->access($this->anonymous));
+
+    $route5 = Url::fromRoute('entity.product_bundle.collection');
+    static::assertTrue($route5->access($this->administrator));
+    static::assertFalse($route5->access($this->developer));
+    static::assertFalse($route5->access($this->anonymous));
 
     // Make sure only the admin account has access to all admin routes.
-    foreach ($admin_routes as $route) {
-      static::assertTrue($route->access($this->administrator));
-      static::assertFalse($route->access($this->developer));
-      static::assertFalse($route->access($this->anonymous));
-    }
+    // foreach ($admin_routes as $route) {
+    //   static::assertTrue($route->access($this->administrator));
+    //   static::assertFalse($route->access($this->developer));
+    //   static::assertFalse($route->access($this->anonymous));
+    // }
   }
 
   /**
