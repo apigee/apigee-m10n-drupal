bf8252138a2ecc93ba3492040f0643fc82ad2d74
Kedar Khaire <kwkhaire11@gmail.com>
2025-03-20 20:35:01 +0530

    Changes for segmentation error and abstract warning

--- tests/src/Kernel/Plugin/Field/FieldWidget/BaseWidgetKernelTest.php
+++ tests/src/Kernel/Plugin/Field/FieldWidget/BaseWidgetKernelTest.php
@@ -28,7 +28,7 @@ use Drupal\field\Entity\FieldStorageConfig;
 /**
  * Base test class for field widget tests.
  */
-abstract class BaseWidgetKernelTest extends MonetizationKernelTestBase {
+class BaseWidgetKernelTest extends MonetizationKernelTestBase {
 
   use ContentTypeCreationTrait;
   use NodeCreationTrait;
