9a200f6a6b71c24eece4b4b56138bdac20e3c2ae
Kedar Khaire <kwkhaire11@gmail.com>
2025-03-24 13:24:53 +0530

    Revert tests

--- tests/src/Kernel/Plugin/Field/FieldWidget/BaseWidgetKernelTest.php
+++ tests/src/Kernel/Plugin/Field/FieldWidget/BaseWidgetKernelTest.php
@@ -28,7 +28,7 @@ use Drupal\field\Entity\FieldStorageConfig;
 /**
  * Base test class for field widget tests.
  */
-class BaseWidgetKernelTest extends MonetizationKernelTestBase {
+abstract class BaseWidgetKernelTest extends MonetizationKernelTestBase {
 
   use ContentTypeCreationTrait;
   use NodeCreationTrait;
