06b7efffd9f02a7ac66276fd6574d71b27472b50
Kedar Khaire <kwkhaire11@gmail.com>
2025-06-04 17:55:39 +0530

    Class name update for product name and total

--- modules/apigee_m10n_add_credit/tests/src/Functional/AddCreditFunctionalTestBase.php
+++ modules/apigee_m10n_add_credit/tests/src/Functional/AddCreditFunctionalTestBase.php
@@ -92,8 +92,8 @@ class AddCreditFunctionalTestBase extends MonetizationFunctionalTestBase {
       'payment_information[add_payment_method][billing_information][address][0][address][postal_code]' => '94043',
     ], 'Continue to review');
     $this->assertCssElementContains('h1.page-title', 'Review');
-    // $this->assertCssElementContains('.checkout-order-summary', $product->label());
-    // $this->assertCssElementContains('.checkout-order-summary', "Total $$amount");
+    $this->assertCssElementContains('.field--name-title', $product->label());
+    $this->assertCssElementContains('.order-total-line__total', "Total $" . $amount);
 
     // Before finalizing the payment, we have to add a couple of responses to
     // the queue.
