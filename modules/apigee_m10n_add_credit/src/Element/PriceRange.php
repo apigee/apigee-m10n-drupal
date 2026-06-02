670795f2250f51c2f53d1a2a0bd70720c6d3c040
Shishir Suvarna <shishirsuvarna@google.com>
2026-06-02 19:06:19 +0530

    update PriceRange element to extend FormElementBase

--- modules/apigee_m10n_add_credit/src/Element/PriceRange.php
+++ modules/apigee_m10n_add_credit/src/Element/PriceRange.php
@@ -21,7 +21,7 @@
 namespace Drupal\apigee_m10n_add_credit\Element;
 
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Render\Element\FormElement;
+use Drupal\Core\Render\Element\FormElementBase;
 
 /**
  * Provides a price range form element.
@@ -47,7 +47,7 @@ use Drupal\Core\Render\Element\FormElement;
  *
  * @FormElement("price_range")
  */
-class PriceRange extends FormElement {
+class PriceRange extends FormElementBase {
 
   /**
    * {@inheritdoc}
